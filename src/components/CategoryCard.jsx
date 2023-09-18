5430c1b4390cb737803efa5d69ec995576306f9b
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-09-18 23:24:58 +0200

    Added arrow

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { CardContainer, TitleContainer } from "./categoryCardStyle";
+import React, { useState } from "react";
+import { Arrow, CardContainer, TitleContainer } from "./categoryCardStyle";
+import DownArrow from "../assets/Arrow-Down.png";
+import UpArrow from "../assets/Arrow-Up.png";
 
 export const CategoryCard = ({ id, title, img, icon }) => {
+	const [expanded, setExpanded] = useState(false);
+
+	const toggleExpanded = () => {
+		setExpanded(!expanded);
+	};
+
 	return (
-		<CardContainer key={id}>
+		<CardContainer key={id} expanded={expanded} onClick={toggleExpanded}>
 			<TitleContainer>
-				<img src={icon} alt="" srcset="" />
+				<img src={icon} alt="" />
 				<h2>{title}</h2>
 			</TitleContainer>
+			<Arrow
+				src={expanded ? UpArrow : DownArrow}
+				alt="Arrow"
+				className="arrow"
+			/>
+			{expanded && (
+				<div className="expanded-content">
+					{/* Expanded content placeholder */}
+					add content pls
+				</div>
+			)}
 		</CardContainer>
 	);
 };
--- src/components/categoryCardStyle.js
+++ src/components/categoryCardStyle.js
@@ -3,19 +3,39 @@ import styled from "styled-components";
 export const CardContainer = styled.div`
 	background-color: #f8f8f8;
 	border-radius: 15px;
-	height: 150px;
+	overflow: hidden;
+	transition: height 0.3s ease;
 	padding: 15px;
 	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
+	cursor: pointer;
+	display: flex;
+	flex-direction: column;
+	height: auto;
+
+	&:hover {
+		box-shadow: 0 4px 6px rgba(0, 0, 0, 0.4);
+	}
 `;
 
 export const TitleContainer = styled.div`
 	display: flex;
 	align-items: center;
 	gap: 30px;
-	overflow: hidden;
-	height: 100%;
+	padding: 10px;
 	img {
 		width: 100px;
 		border-radius: 15px;
 	}
+`;
+
+export const ArrowContainer = styled.div`
+	display: flex;
+	align-self: center;
+`;
+
+export const Arrow = styled.img`
+	position: sticky;
+
+	width: 40px;
+	height: 40px;
 `;

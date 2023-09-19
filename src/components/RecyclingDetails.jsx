7af6667e1585b52e32a2181cd49ca8f9c3df490a
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-09-19 13:45:26 +0200

    Fetchd correct detail data

--- src/components/RecyclingDetails.jsx
+++ src/components/RecyclingDetails.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const RecyclingDetails = () => {
+	const [orgData, setOrgData] = useState();
+	const [reviewData, setReviewData] = useState();
+	const [loading, setLoading] = useState(true);
 	const { org_id } = useParams();
 
-	return <div>{org_id}</div>;
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					`http://localhost:3000/orgs/${org_id}`
+				);
+				const data = response.data;
+				setOrgData(data);
+			} catch (error) {
+				console.error("Error fetching data: ", error);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get(
+					`http://localhost:3000/reviews/${org_id}`
+				);
+				const data = response.data;
+				setReviewData(data);
+			} catch (error) {
+				console.error("Error fetching data: ", error);
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		if (orgData && reviewData) {
+			setLoading(false);
+			// console.log("loading complete");
+		}
+	}, [orgData, reviewData]);
+
+	return <div>{loading ? <div>Loading...</div> : <div>{org_id}</div>}</div>;
 };

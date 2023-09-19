f971ea4f023ee866ad9910bd2ac475730531a1eb
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-09-19 11:12:18 +0200

    Added proper titles

--- src/Title.js
+++ src/Title.js
@@ -5,26 +5,24 @@ export const Title = () => {
 	const location = useLocation();
 
 	useEffect(() => {
-		switch (
-			true
-			// case location.pathname === "/":
-			// 	document.title = "Hotel Overlook - Forside";
-			// 	break;
-			// case location.pathname.startsWith("/destinations"):
-			// 	document.title = "Hotel Overlook - Hoteller";
-			// 	break;
-			// case location.pathname === "/reservations":
-			// 	document.title = "Hotel Overlook - Reservationer";
-			// 	break;
-			// case location.pathname === "/login":
-			// 	document.title = "Hotel Overlook - Login";
-			// 	break;
-			// case location.pathname === "/rooms":
-			// 	document.title = "Hotel Overlook - Værelser";
-			// 	break;
-			// default:
-			// 	document.title = "Overlook";
-		) {
+		switch (true) {
+			case location.pathname === "/":
+				document.title = "Affaldsguiden - Forside";
+				break;
+			case location.pathname === "/sorting":
+				document.title = "Affaldsguiden - Sortering";
+				break;
+			case location.pathname === "/recycling":
+				document.title = "Affaldsguiden - Genbrugsstationer";
+				break;
+			case location.pathname === "/purchase":
+				document.title = "Hotel Overlook - Bestil Holder";
+				break;
+			case location.pathname === "/login":
+				document.title = "Hotel Overlook - Log ind";
+				break;
+			default:
+				document.title = "Affaldsguiden";
 		}
 	}, [location.pathname]);
 

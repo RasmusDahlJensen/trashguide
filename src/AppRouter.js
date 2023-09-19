7a74361ccd11e1f4cbd6930c7d0c8ff054c1ff49
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-09-19 11:13:06 +0200

    Added not found page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,6 +3,7 @@ import { Home } from "./pages/Home";
 import { Sorting } from "./pages/Sorting";
 import { SectionDetail } from "./components/SectionDetail";
 import { Recycling } from "./pages/Recycling";
+import { NotFound } from "./pages/404";
 
 export const AppRouter = () => {
 	return (
@@ -11,6 +12,7 @@ export const AppRouter = () => {
 			<Route path="/sorting" element={<Sorting />} />
 			<Route path="/sorting/:section_id" element={<SectionDetail />} />
 			<Route path="/recycling/" element={<Recycling />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 };
